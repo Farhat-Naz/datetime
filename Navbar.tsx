8b80168f5827bdf60a50b67abbf373351d20e681
Farhat Naz <159726939+Farhat-Naz@users.noreply.github.com>
2024-11-09 01:27:47 +0500

    Add files via upload

---
+++ Navbar.tsx
@@ -0,0 +1,94 @@
+"use client"
+import React from 'react'
+import { useEffect, useState } from 'react'
+import Link from "next/link"
+import { NodeNextRequest } from 'next/dist/server/base-http/node';
+
+
+export default function Navbar() {
+
+    const [currentDateTime, setCurrentDateTime]=useState('');
+    useEffect(()=>{
+        const updateDateTime = ()=>{
+            const now=new Date();
+            const option:Intl.DateTimeFormatOptions ={
+                weekday: 'long',
+                year: 'numeric',
+                month: 'long',
+                day:'numeric',
+                hour:'2-digit',
+                minute: '2-digit',
+                second: '2-digit',
+                hour12: false
+
+
+    
+}
+            setCurrentDateTime(now.toLocaleDateString('en-us', option) + '' + now.toLocaleTimeString('en-us', {hour12:false}) 
+);
+    };
+
+    updateDateTime()
+      const intValId= setInterval(updateDateTime, 1000);
+return()=>clearInterval(intValId)
+    }, []);
+
+
+
+
+    
+  return (
+
+    <header className="text-gray-400 body-font shadow-lg">
+    <div className="container mx-auto flex flex-wrap p-2 flex-col md:flex-row items-center">
+        <div className="flex-1 text-black">
+            <span >{currentDateTime}
+
+            </span>
+
+        </div>
+      
+<nav className="md:ml-auto flex flex-wrap items-center text-black justify-center">
+    <Link href="http://localhost:3000/" className="mr-5 hover:text-red-700 transition-colors durataion-300 transform hover:scale-10">
+{" "}Home {" "}
+    </Link>
+    <Link href="http://localhost:3000/" className="mr-5 hover:text-red-700 transition-colors durataion-300 transform hover:scale-10">
+{" "}About {" "}
+    </Link>
+    <Link href="http://localhost:3000/" className="mr-5 hover:text-red-700 transition-colors durataion-300 transform hover:scale-10">
+{" "}Contacts {" "}
+    </Link>
+    <Link href="http://localhost:3000/" className="mr-5 hover:text-red-700 transition-colors durataion-300 transform hover:scale-10">
+{" "}Blog {" "}
+    </Link>
+
+    <button className="inline-flex items-center bg-blue-400 border-1 py-1 px-3 focus:outline-none hover:bg-red-700 rounded text-white mt-4 md:ml-0 transition-colors duration-300 ">
+        Github
+        <svg fill='none'
+        stroke='currentColor'
+        strokeLinecap='round'
+        strokeLinejoin="round"
+        strokeWidth={2}
+        className="w-4 h-4 ml-1"
+        viewBox="0 0 24 24"
+        >
+            <path d=" MS 12h14M12 S17 7-7 7"/>
+
+        </svg>
+    </button>
+</nav>
+
+
+
+
+
+
+
+
+
+
+    </div>
+    </header>
+  ) 
+}
+
---
+++ page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from "./components/Navbar"
+
+function Home() {
+  return (
+    <div>
+      Farhat's blog
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Home
